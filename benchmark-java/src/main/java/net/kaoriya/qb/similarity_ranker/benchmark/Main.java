867e3814a2bb75abfd86e7a3cbe979ec8ad45289
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-02-27 13:51:02 +0900

    implement setup procedure

--- benchmark-java/src/main/java/net/kaoriya/qb/similarity_ranker/benchmark/Main.java
+++ benchmark-java/src/main/java/net/kaoriya/qb/similarity_ranker/benchmark/Main.java
@@ -1,9 +1,83 @@
 package net.kaoriya.qb.similarity_ranker.benchmark;
 
+import java.util.Set;
+import java.util.Random;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static final String PREFIX = "bench_item_";
+    public static final String KEYS_KEY = "bench_keys";
+
+    public static String generateKey(String prefix, int n)
     {
-        System.out.println("Hello World!");
+        return prefix + n;
+    }
+
+    public static byte[] generateValue(Random r, int size) throws Exception
+    {
+        byte[] b = new byte[size];
+        r.nextBytes(b);
+        return b;
+    }
+
+    public static void setBits(
+            Jedis jedis,
+            String key,
+            byte[] value)
+    {
+        jedis.del(key);
+        for (int i = 0; i < value.length; ++i) {
+            int n = (int)value[i] & 0xFF;
+            for (int j = 0; j < 8; ++j) {
+                if ((n & (0x80 >> j)) != 0) {
+                    jedis.setbit(key, i * 8 + j, true);
+                }
+            }
+        }
+    }
+
+    public static void setupData(
+            Jedis jedis,
+            int size,
+            int count,
+            String keysKey)
+        throws Exception
+    {
+        jedis.del(keysKey);
+        Random r = new Random();
+        for (int i = 0; i < count; ++i)
+        {
+            String key = generateKey(PREFIX, i);
+            byte[] value = generateValue(r, size);
+            setBits(jedis, key, value);
+            jedis.lpush(keysKey, key);
+        }
+    }
+
+    public static void benchmark(
+            String name,
+            String host,
+            int size,
+            int count)
+        throws Exception
+    {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), host);
+        Jedis jedis = pool.getResource();
+        try {
+            setupData(jedis, size, count, KEYS_KEY);
+            Set<String> keys = jedis.keys("*");
+        } finally {
+            pool.returnResource(jedis);
+        }
+        pool.destroy();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        benchmark("first", "avalon", 64, 1000);
     }
 }
